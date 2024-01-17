52d7bb92f7ecef4a095a808937dda90e571e73d5
duboimax <maxencedubois22@gmail.com>
2024-01-17 16:39:19 +0100

    Normalement c'est finis

--- Document App/DocumentTableViewController.swift
+++ Document App/DocumentTableViewController.swift
@@ -5,14 +5,15 @@
 //  Created by Maxence DUBOIS on 1/17/24.
 //
 import UIKit
+import QuickLook
 
 extension Int {
     func formattedSize(int size: Int) -> String {
         let formatter = ByteCountFormatter.string(fromByteCount: Int64(size), countStyle: .file)
         return formatter
-        
     }
 }
+
 
 struct DocumentFile {
     var title: String
@@ -33,118 +34,228 @@ func listFileInBundle() -> [DocumentFile] {
     let path = Bundle.main.resourcePath! // Indication du chemin vers DocumentApp
     let items = try! fm.contentsOfDirectory(atPath: path) // Récupération de toutes les images
     
-    var documentListBundle = [DocumentFile]() // On définit une liste vide de type: DocumentFile
+    
+    var bundleDocuments: [DocumentFile] = []
     
     for item in items { // Boucle sur chaque image
         if !item.hasSuffix("DS_Store") && item.hasSuffix(".jpg") { // On filtre les images
             let currentUrl = URL(fileURLWithPath: path + "/" + item) // On récupére les propriétés de l'image
             let resourcesValues = try! currentUrl.resourceValues(forKeys: [.contentTypeKey, .nameKey, .fileSizeKey])
             // On récupère les valeurs des propriétés de l'image
-            documentListBundle.append(DocumentFile(
+            bundleDocuments.append(DocumentFile(
                 title: resourcesValues.name!,
                 size: resourcesValues.fileSize ?? 0,
                 imageName: item,
                 url: currentUrl,
                 type: resourcesValues.contentType!.description) // On crée un objet DocumentFile avec les données que nous avons
-                                      // récupéré
+                                   // récupéré
             )
         }
     }
-    return documentListBundle
+    return bundleDocuments
+}
+
+func listFileInDocumentApplication() -> [DocumentFile] {
+    let fm = FileManager.default
+    var importedDocuments: [DocumentFile] = []
+    
+    if let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
+        do {
+            let items = try fm.contentsOfDirectory(atPath: documentsDirectory.path)
+            for item in items {
+                if !item.hasSuffix("DS_Store") && item.hasSuffix(".jpg") {
+                    let currentUrl = documentsDirectory.appendingPathComponent(item)
+                    let resourcesValues = try currentUrl.resourceValues(forKeys: [.contentTypeKey, .nameKey, .fileSizeKey])
+                    
+                    importedDocuments.append(DocumentFile(
+                        title: resourcesValues.name!,
+                        size: resourcesValues.fileSize ?? 0,
+                        imageName: item,
+                        url: currentUrl,
+                        type: resourcesValues.contentType!.description)
+                    )
+                }
+            }
+        } catch {
+            print("Erreur lors de la lecture des fichiers : \(error)")
+        }
+    }
+    
+    return importedDocuments
 }
 
 
 
+class DocumentTableViewController: UITableViewController, QLPreviewControllerDataSource, UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+            let searchBar = searchController.searchBar
+            filterContentForSearchText(searchBar.text!)
+        }
+    
+    
+    var listDocumentBundle: [DocumentFile] = []
+    var listDocumentImported: [DocumentFile] = []
+    
+    var searchController: UISearchController!
+    var filteredBundleDocuments: [DocumentFile] = []
+    var filteredImportedDocuments: [DocumentFile] = []
 
-class DocumentTableViewController: UITableViewController {
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(importDocument))
+        listDocumentBundle = listFileInBundle()
+        listDocumentImported = listFileInDocumentApplication()
         
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
+        searchController = UISearchController(searchResultsController: nil)
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = "Search Documents"
+        navigationItem.searchController = searchController
+        definesPresentationContext = true
         
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
     
     // MARK: - Table view data source
     
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 1
+        return 2 // Deux sections : Bundle et Importations
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return section == 0 ? "Bundle" : "Importations"
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return DocumentFile.data.count
+        if isFiltering() {
+            return section == 0 ? filteredBundleDocuments.count : filteredImportedDocuments.count
+        }
+        return section == 0 ? listDocumentBundle.count : listDocumentImported.count
     }
-    
-    
+
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "DocumentCell", for: indexPath)
-        
-        let element = DocumentFile.data[indexPath.row]
-        
-        cell.textLabel?.text = element.title
-        cell.detailTextLabel?.text = element.size.formattedSize(int: element.size)
+        let document: DocumentFile
+
+        if isFiltering() {
+            document = indexPath.section == 0 ? filteredBundleDocuments[indexPath.row] : filteredImportedDocuments[indexPath.row]
+        } else {
+            document = indexPath.section == 0 ? listDocumentBundle[indexPath.row] : listDocumentImported[indexPath.row]
+        }
+
+        cell.textLabel?.text = document.title
+        cell.detailTextLabel?.text = document.size.formattedSize(int: document.size)
         
         return cell
     }
     
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if let cell = sender as? UITableViewCell, let indexPath = tableView.indexPath(for: cell), let documentVC = segue.destination as? DocumentViewController {
-            let selectedDocument = DocumentFile.data[indexPath.row]
-            documentVC.imageName = selectedDocument.imageName
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let previewController = QLPreviewController()
+        previewController.dataSource = self
+        
+        if indexPath.section == 0 {
+            // Si l'élément sélectionné est dans la section Bundle
+            previewController.currentPreviewItemIndex = indexPath.row
+        } else {
+            // Si l'élément sélectionné est dans la section Importations
+            // Ajuster l'index en prenant en compte la taille de la première section
+            previewController.currentPreviewItemIndex = listDocumentBundle.count + indexPath.row
+        }
+        
+        navigationController?.pushViewController(previewController, animated: true)
+    }
+    
+    // MARK: - QLPreviewControllerDataSource
+    
+    func numberOfPreviewItems(in controller: QLPreviewController) -> Int {
+        return listDocumentBundle.count +  listDocumentImported.count
+    }
+    
+    func previewController(_ controller: QLPreviewController, previewItemAt index: Int) -> QLPreviewItem {
+        // Calculer l'index réel en fonction du nombre d'éléments dans la première section
+        if index < listDocumentBundle.count {
+            // L'index appartient à la première section (Bundle)
+            let document = listDocumentBundle[index]
+            return document.url as QLPreviewItem
+        } else {
+            // L'index appartient à la deuxième section (Importations)
+            let adjustedIndex = index - listDocumentBundle.count
+            let document = listDocumentImported[adjustedIndex]
+            return document.url as QLPreviewItem
         }
     }
     
+    @objc func importDocument() {
+        // Implémentez ici la logique d'importation de documents
+        // Par exemple, ouvrir un UIDocumentPickerViewController pour sélectionner des fichiers
+        let documentPicker = UIDocumentPickerViewController(documentTypes: ["public.content"], in: .import)
+        documentPicker.delegate = self
+        documentPicker.modalPresentationStyle = .formSheet
+        self.present(documentPicker, animated: true, completion: nil)
+    }
     
+    func copyFileToDocumentsDirectory(fromUrl url: URL) {
+        let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        let destinationUrl = documentsDirectory.appendingPathComponent(url.lastPathComponent)
+        
+        do {
+            if FileManager.default.fileExists(atPath: destinationUrl.path) {
+                try FileManager.default.removeItem(at: destinationUrl)
+            }
+            try FileManager.default.copyItem(at: url, to: destinationUrl)
+        } catch {
+            print("Erreur lors de la copie du fichier : \(error)")
+        }
+    }
     
+    func filterContentForSearchText(_ searchText: String) {
+        filteredBundleDocuments = listDocumentBundle.filter { document in
+            return document.title.lowercased().contains(searchText.lowercased())
+        }
+        filteredImportedDocuments = listDocumentImported.filter { document in
+            return document.title.lowercased().contains(searchText.lowercased())
+        }
+        
+        tableView.reloadData()
+    }
+
     
-    /*
-     // Override to support conditional editing of the table view.
-     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-     // Return false if you do not want the specified item to be editable.
-     return true
-     }
-     */
-    
-    /*
-     // Override to support editing the table view.
-     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-     if editingStyle == .delete {
-     // Delete the row from the data source
-     tableView.deleteRows(at: [indexPath], with: .fade)
-     } else if editingStyle == .insert {
-     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-     }
-     }
-     */
-    
-    /*
-     // Override to support rearranging the table view.
-     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-     
-     }
-     */
-    
-    /*
-     // Override to support conditional rearranging of the table view.
-     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-     // Return false if you do not want the item to be re-orderable.
-     return true
-     }
-     */
-    
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
-     }
-     */
-    
+    func isSearchBarEmpty() -> Bool {
+        return searchController.searchBar.text?.isEmpty ?? true
+    }
+
+    func isFiltering() -> Bool {
+        return searchController.isActive && !isSearchBarEmpty()
+    }
+
 }
+
+extension DocumentTableViewController: UIDocumentPickerDelegate {
+    func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentsAt urls: [URL]) {
+        guard let selectedUrl = urls.first else { return }
+        
+        do {
+            let resourcesValues = try selectedUrl.resourceValues(forKeys: [.contentTypeKey, .nameKey, .fileSizeKey])
+            let newDocument = DocumentFile(
+                title: resourcesValues.name!,
+                size: resourcesValues.fileSize ?? 0,
+                imageName: nil,
+                url: selectedUrl,
+                type: resourcesValues.contentType!.description
+            )
+            
+            // Copier le fichier dans le répertoire des documents
+            copyFileToDocumentsDirectory(fromUrl: selectedUrl)
+            
+            // Ajouter le nouveau document à la source de données
+            listDocumentImported = listFileInDocumentApplication()
+            
+            // Mettre à jour la vue tableau
+            tableView.reloadData()
+        } catch {
+            print("Erreur lors de la récupération des informations du document : \(error)")
+        }
+    }
+}
+
